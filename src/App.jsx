f26192254386f838bff3b82a91b7f90840cc1a92
abu bakar <abubakar850772@gmail.com>
2024-08-27 23:00:29 +0600

    finished project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react"
+
+function App() {
+
+  return (
+    <>
+    <h1 className=" text-4xl font-medium bg-yellow-700 p-12">Tailwind css</h1>
+    <Button>Clck me</Button>
+    </>
+  )
+}
+
+export default App

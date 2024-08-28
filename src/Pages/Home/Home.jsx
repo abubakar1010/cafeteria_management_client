6aedbaa3b8b0fdf2056b63494663741a5cff5095
abu bakar <abubakar850772@gmail.com>
2024-08-28 14:51:04 +0600

    finished create menu item

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../Components/Banner/Banner";
+import Menu from "../../Components/Menu/Menu";
 import OrderCategory from "../../Components/OrderCategory/OrderCategory";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Banner />
             <OrderCategory />
+            <Menu />
         </div>
     );
 };
---
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,53 @@
+import {
+    Card,
+    CardHeader,
+    Typography,
+    Avatar,
+  } from "@material-tailwind/react";
+   
+
+// eslint-disable-next-line react/prop-types
+const MenuItem = ({item}) => {
+
+
+    // eslint-disable-next-line react/prop-types
+    const {image, price, name, recipe} = item;
+    return (
+        <>
+        <Card color="transparent" shadow={true} className="w-full ">
+        <CardHeader
+          color="transparent"
+          floated={false}
+          shadow={false}
+          className=" flex items-center gap-4 p-4"
+        >
+          <Avatar
+            size="xl"
+            variant="circular"
+            src={image}
+            alt="tania andrew"
+            className=" !rounded-tl-[200px] "
+          />
+          <div className="flex w-full flex-col gap-0.5">
+            <div className="flex items-center justify-between">
+              <p className=" text-[#151515] text-xl ">
+                {name} - - - - - - - - - -
+              </p>
+              <div className=" text-[#D99904] flex items-center gap-0">
+                ${price}
+              </div>
+            </div>
+            <Typography color="blue-gray" className=" max-w-sm mt-3">{recipe}</Typography>
+          </div>
+        </CardHeader>
+
+      </Card>
+        </>
+    );
+};
+
+export default MenuItem;
+
+
+
+   

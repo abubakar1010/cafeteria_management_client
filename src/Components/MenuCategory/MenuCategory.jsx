07fc3672771e0de95c01ae14d2f7dc90669c2f0b
abu bakar <abubakar850772@gmail.com>
2024-08-28 23:25:41 +0600

    finished menu category section

--- src/Components/MenuCategory/MenuCategory.jsx
+++ src/Components/MenuCategory/MenuCategory.jsx
@@ -1,28 +1,35 @@
 import { Button } from "@material-tailwind/react";
 import MenuItem from "../../Pages/Shared/MenuItem/MenuItem";
 import Cover from "../Cover/Cover";
+import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
-const MenuCategory = ({item, img, coverTitle, coverDescription}) => {
-    console.log(item);
-    
-    return (
-        <>
-        {img && <Cover img={img} title={coverTitle} description={coverDescription} />}
-         <div className=" grid grid-cols-2 gap-12 justify-center items-center mt-24 ">
+const MenuCategory = ({ item, img, coverTitle, coverDescription }) => {
+//   console.log(item);
+
+  return (
+    <>
+      {img && (
+        <Cover img={img} title={coverTitle} description={coverDescription} />
+      )}
+      <div className=" grid grid-cols-2 gap-12 justify-center items-center mt-24 ">
         {
+          // eslint-disable-next-line react/prop-types
+          item.map((item) => (
             // eslint-disable-next-line react/prop-types
-            item.map( item => <MenuItem key={item._id} item={item} />)
+            <MenuItem key={item._id} item={item} />
+          ))
         }
       </div>
       <div className=" text-center mb-28 mt-12">
-      <Button
-       className=" text-[#1F2937] uppercase text-xl rounded-md border-b-2 border-[#1F2937] bg-[#FFF] " >View Full Menu</Button>
-
+        <Link to={`/order/${coverTitle}`}>
+          <Button className=" text-[#1F2937] uppercase text-xl rounded-md border-b-2 border-[#1F2937] bg-[#FFF] ">
+            View Full Menu
+          </Button>
+        </Link>
       </div>
-        </>
-
-    );
+    </>
+  );
 };
 
 export default MenuCategory;

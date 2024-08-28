38b6c9157d499c3c79453ca2fd04b49d51e65158
abu bakar <abubakar850772@gmail.com>
2024-08-28 13:59:22 +0600

    finished order category section

---
+++ src/Components/OrderCategory/OrderCategory.jsx
@@ -0,0 +1,69 @@
+
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import './styles.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+// import image 
+
+import slide1 from '../../assets/home/slide1.jpg'
+import slide2 from '../../assets/home/slide2.jpg'
+import slide3 from '../../assets/home/slide3.jpg'
+import slide4 from '../../assets/home/slide4.jpg'
+import slide5 from '../../assets/home/slide5.jpg'
+import SectionTitle from '../SectionTitle/SectionTitle';
+
+const OrderCategory = () => {
+    return (
+        <>
+
+        <SectionTitle subHeading={'From 11:00am to 10:00pm'} heading={'ORDER ONLINE'} />
+
+        <div className=' mb-24'>
+            <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={false}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <img src={slide1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide4} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide5} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide3} alt="" />
+        </SwiperSlide>
+
+      </Swiper>
+        </div>
+        </>
+    );
+};
+
+export default OrderCategory;

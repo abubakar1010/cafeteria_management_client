9f7bf5675f778d22ec4998811d16a24be9bbabb7
abu bakar <abubakar850772@gmail.com>
2024-08-31 22:20:49 +0600

    fixed sign up and sign in page layout redirect issues

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,19 +1,13 @@
 import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "../Pages/Shared/NavBar/NavBar";
 import Footer from "../Pages/Shared/Footer/Footer";
-import { useContext } from "react";
-import { AuthContext } from "../Provider/AuthProvider";
 
 const Layout = () => {
 
     const location = useLocation()
-    const {isLoading} = useContext(AuthContext)
 
     const isAutLayOut = location?.pathname?.includes('login') || location?.pathname?.includes('register')
 
-    // console.log(isAutLayOut);
-
-    if(isLoading) return
 
 
     if(isAutLayOut) return <Outlet />
